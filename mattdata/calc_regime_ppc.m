a07b7c07907b84b3bb36f466fccb66f89aa1423f
Graham Smith <grahamas@gmail.com>
2015-07-10 13:35:03 -0500

    Added regime spike times.

--- mattdata/calc_regime_ppc.m
+++ mattdata/calc_regime_ppc.m
@@ -27,6 +27,7 @@ parfor ii = 1:num_channels
             
             consistency = zeros(num_behaviors, 1);
             regime_spike_angles = cell(num_behaviors, 1);
+            regime_spike_times = cell(num_behaviors, 1);
             for ll = 1:num_behaviors
                 timestamps = all_channels(ii).behavior_spectra(ll).(regime).time;
                 start = timestamps(1);
@@ -35,17 +36,22 @@ parfor ii = 1:num_channels
                 % save some space
                 all_channels(ii).behavior_spectra(ll).(regime).time = [start, stop];
                 
-                these_spike_angles = spike_angles(spike_times >= start &...
-                    spike_times < stop);
+                these_spike_dx = (spike_times >= start & spike_times < stop);
+                these_spike_angles = spike_angles(these_spike_dx);
+                these_spike_times = spike_times(these_spike_dx);
+                
                 
                 regime_spike_angles{ll} = these_spike_angles;
+                regime_spike_times{ll} = these_spike_times;
                 consistency(ll) = ppc_from_spike_angles(these_spike_angles);
             end
             all_channels(ii).unit_waveforms(jj).regime_spike_angles.(regime) = regime_spike_angles;
+            all_channels(ii).unit_waveforms(jj).regime_spike_times.(regime) = regime_spike_times;
             all_channels(ii).unit_waveforms(jj).regime_ppcs.(regime) = consistency;
         end
     end
 end
+'...done.'
        
 save('all_channels.mat', 'all_channels', '-v7.3')
             
