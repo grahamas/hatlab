872a9c5292d1731fa2a31430c124a3a35f5e4ee7
Graham A Smith <grahams@midway-login2.rcc.local>
2015-07-13 15:39:52 -0500

    Made switch of regime defns.

--- mattdata/calc_regime_ppc.m
+++ mattdata/calc_regime_ppc.m
@@ -1,25 +1,33 @@
 if ~exist('all_channels', 'var')
-    load('all_channels_ppc.mat');
+    load('all_channels_new.mat');
+end
+
+if ~exist('beh', 'var')
+    load('beh.mat');
 end
 
 lfp_fs = 1000;
 spike_fs = 30000;
 
 num_channels = length(all_channels);
+        
+
 
 parfor ii = 1:num_channels
     if isempty(all_channels(ii).unit_waveforms)
         continue
     end
     
-    regimes = {'before', 'delay', 'go', 'movement',...
-        'after_reward', 'gross'}
+    regimes = {'instruction_early_sec', 'instruction_late_sec', 'execution_sec'}
+    
     num_regimes = length(regimes);
     num_behaviors = length(all_channels(ii).behavior_spectra);
     
     for jj = 1:length(all_channels(ii).unit_waveforms)
         temp_unit = all_channels(ii).unit_waveforms(jj);
-        spike_times = temp_unit.timestamp * lfp_fs ; %%% HERE BE PROBLEM
+        spike_times = temp_unit.timestamp ; %%% HERE BE PROBLEM
+                    %%%% NOTE THAT THIS ASSUMES REGIMES IN SECONDS
+                    %%%% NOT THE CASE!!!!
         spike_angles = temp_unit.spike_angles;
         
         for kk = 1:num_regimes
@@ -28,13 +36,11 @@ parfor ii = 1:num_channels
             consistency = zeros(num_behaviors, 1);
             regime_spike_angles = cell(num_behaviors, 1);
             regime_spike_times = cell(num_behaviors, 1);
+            regime_spike_rates = cell(num_behaviors, 1);
             for ll = 1:num_behaviors
                 timestamps = all_channels(ii).behavior_spectra(ll).(regime).time;
                 start = timestamps(1);
-                stop = timestamps(end);
-                
-                % save some space
-                all_channels(ii).behavior_spectra(ll).(regime).time = [start, stop];
+                stop = timestamps(2);
                 
                 these_spike_dx = (spike_times >= start & spike_times < stop);
                 these_spike_angles = spike_angles(these_spike_dx);
@@ -43,10 +49,12 @@ parfor ii = 1:num_channels
                 
                 regime_spike_angles{ll} = these_spike_angles;
                 regime_spike_times{ll} = these_spike_times;
+                regime_spike_rates{ll} = length(these_spike_times) / (stop - start) % SECONDS DEPENDENCE
                 consistency(ll) = ppc_from_spike_angles(these_spike_angles);
             end
             all_channels(ii).unit_waveforms(jj).regime_spike_angles.(regime) = regime_spike_angles;
             all_channels(ii).unit_waveforms(jj).regime_spike_times.(regime) = regime_spike_times;
+            all_channels(ii).unit_waveforms(jj).regime_spike_rates.(regime) = regime_spike_rates
             all_channels(ii).unit_waveforms(jj).regime_ppcs.(regime) = consistency;
         end
     end
