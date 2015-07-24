14293359a39c52b1d260c6a7930c5d6be0e575cf
Graham A Smith <grahams@midway-login2.rcc.local>
2015-07-24 09:56:28 -0500

    Created rs_script. Cleaned up.

---
+++ mattdata/rs_script.m
@@ -0,0 +1,17 @@
+load('definitions')
+
+snr_file = 'rs1050225_clean_SNRgt4.mat';
+lfp_file = 'rs1050225_MI_clean_LFP.mat';
+spike_file = 'rs1050225_MI_SpikeData.mat';
+
+new_session_name = 'session050225.mat';
+new_columns_name = 'analysis_columns_050225.mat';
+
+base_session = parse_rs_files(snr_file, lfp_file, spike_file);
+session = populate_session(base_session, definitions);
+
+save(new_session_name, session);
+
+analysis_columns = get_analysis_columns(session, definitions);
+
+save(new_columns_name, analysis_columns);
