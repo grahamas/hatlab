322a9ee2e1ebce385c79af90e0dce6015e8d7a0f
Graham A Smith <grahamas@gmail.com>
2015-08-25 16:32:41 -0500

    rcc->lptp: Small error.

--- mattdata/@ChannelRecording/ChannelRecording.m
+++ mattdata/@ChannelRecording/ChannelRecording.m
@@ -2,14 +2,19 @@ classdef ChannelRecording < dynamicprops
     
     properties (Constant)
         band_property_prefix = 'LFP_band_';
+        band_timeseries_suffix = '_timeseries';
     end
     
     properties (SetAccess = immutable)
         parent_array
         channel_number
-        LFP
+        LFP_timeseries
     end
     
+    properties (Dependent)
+        LFP
+    end
+
     properties
         unit_list
     end
@@ -18,7 +23,10 @@ classdef ChannelRecording < dynamicprops
         function obj = ChannelRecording(parent_array, channel_number, LFP)
             obj.parent_array = parent_array;
             obj.channel_number = channel_number;
-            obj.LFP = LFP;
+            LFP_fs = parent_array.LFP_fs;
+            LFP_time = 0:(1/LFP_fs):((length(LFP)-1)/LFP_fs);
+            obj.LFP_timeseries = timeseries(LFP, LFP_time);
+            obj.LFP_timeseries.TimeInfo.Units = 'seconds';
             obj.unit_list = [];
         end
         function add_unit(obj, new_unit)
@@ -32,11 +40,21 @@ classdef ChannelRecording < dynamicprops
             end
         end
         function compute_LFP_band(obj, band_name, band_cutoffs)
-            new_property_name = [band_property_prefix, band_name];
+            new_property_name = [obj.band_property_prefix, band_name];
+            new_timeseries_name = [new_property_name, obj.band_timeseries_suffix];
             if ~isprop(new_property_name, obj)
-                obj.addprop(new_property_name)
+                obj.addprop(new_property_name);
+                obj.addprop(new_timeseries_name);
             end
-            obj.(new_property_name) = bandpass_filt(obj.LFP, band_cutoffs);
+            new_data = bandpass_filt(obj.LFP, band_cutoffs);
+            new_timeseries = timeseries(new_data, obj.LFP_timeseries.Time);
+            new_timeseries
+        end
+    end
+
+    methods
+        function data = get.LFP(obj)
+            data = LFP_timeseries.Data
         end
     end
 end
