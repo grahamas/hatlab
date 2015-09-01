9f4373df6e1d5aaeffc336b1c4255e9d4c4f60c5
Graham Smith <grahamas@gmail.com>
2015-09-01 12:25:26 -0500

    Check for prop before adding.

--- mattdata/plot_phase_distributions.m
+++ mattdata/plot_phase_distributions.m
@@ -12,9 +12,11 @@ figure
 n_epochs = length(epoch_name_list);
 subplot(1, n_epochs, 1);
 for i_epoch = 1:n_epochs
-    
+    subplot(1, n_epochs, i_epoch);
+    epoch_name = epoch_name_list{i_epoch};
+    epoch_spike_angles = mod(unit.compute_band_epoch_spike_angles(band_name, epoch_name), 2*pi);
 end
-suptitle['Phase distributions by epoch, 
+suptitle['Phase distributions by epoch, ])
 
 end
 
